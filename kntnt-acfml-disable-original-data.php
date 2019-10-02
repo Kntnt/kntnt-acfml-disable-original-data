bbc831d26927e334681ce1f6d1aedee3a9f039b0
Thomas Barregren <thomas@kntnt.com>
2019-10-02 20:47:12 +0200

    Initial commit.

---
+++ kntnt-acfml-disable-original-data.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt Disabler of Original Data for Advanced Custom Fields Multilingual
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Disables the "Field's value in original language" message under ACF fields that are translated with WPML.
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+defined( 'ABSPATH' ) || die;
+
+add_filter( 'wpml_custom_field_original_data', __return_null(), 11);
